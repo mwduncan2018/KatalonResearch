a467bbf3914f43c99507aa5ada0c6536d18ae7ab
Michael Duncan <mwduncan2018@gmail.com>
2021-05-06 14:35:42 -0400

    updates

--- Test Suites/Test Suite Collections/ShortMain.ts
+++ Test Suites/Test Suite Collections/ShortMain.ts
@@ -3,7 +3,7 @@
    <description></description>
    <name>ShortMain</name>
    <tag></tag>
-   <executionMode>PARALLEL</executionMode>
+   <executionMode>SEQUENTIAL</executionMode>
    <maxConcurrentInstances>8</maxConcurrentInstances>
    <testSuiteRunConfigurations>
       <TestSuiteRunConfiguration>
